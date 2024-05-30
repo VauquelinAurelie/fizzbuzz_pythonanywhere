7f395b822c4aad0fa70ec4c44578ecb5a24dc510
Vauquelin Aurelie <aurelie.vauquelin@le-campus-numerique.fr>
2024-05-30 09:20:30 +0200

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+def calculate_fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return number
+
+
+def main():
+    for number in range(1, 101):
+        print(calculate_fizzbuzz(number))
+
+
+if __name__ == "__main__":
+    main()
+
